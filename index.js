f25dad1f1d19198b103eeee4f59a4a7667b568b7
Kaushal <kaushallokhande12345@gamil.com>
2025-01-24 23:04:18 +0530

    rename server.js to index.js and update start script in package.json

--- index.js
+++ index.js
@@ -12,10 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
+mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log('Connected to MongoDB');
 }).catch((error) => {
     console.error('MongoDB connection failed:', error.message);
